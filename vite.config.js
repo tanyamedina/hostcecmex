df43ed95a8524936bb9c4df001635d41a80f076b
tanyamedina <criaturamaldita@gmail.com>
2023-11-18 17:03:19 -0700

    out dir: dist

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    base: '/hostcecmex/', // Ajusta la configuración de la URL base
+  },
+  build: {
+    outDir: 'dist', // Asegúrate de que coincida con el nombre de tu carpeta de construcción
+  },
+  base: '/hostcecmex/', // Reemplaza con el nombre de tu repositorio en GitHub
 })
+
